afe65e7f335e88f68cde5246667ce590e339cd6d
Lee Giles <hebrews110@gmail.com>
2020-06-21 19:23:05 -0400

    Finalizing project.

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ $draw.on('click', function() {
     $("#playerAnswer").css("display", "block");
   }
 
-  $('input').val('');
+  $('input').val(''); //This empties out the input box.
 
     assign();
 })
